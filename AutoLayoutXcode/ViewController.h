480980f375c34791e3216e26194d66018a891ea8
JakeQiu3 <qsy118614@126.com>
2016-03-24 18:10:51 +0800

    init布局

---
+++ AutoLayoutXcode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutXcode
+//
+//  Created by qsy on 14/12/10.
+//  Copyright (c) 2014年 qsy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AutoLayoutXcode/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  AutoLayoutXcode
+//
+//  Created by qsy on 14/12/10.
+//  Copyright (c) 2014年 qsy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+//    http://blog.csdn.net/pucker/article/details/41843511
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
